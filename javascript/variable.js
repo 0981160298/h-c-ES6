6542eb171f5253103c0abd5468dbd5d47b47f212
0981160298 <62273292+0981160298@users.noreply.github.com>
2021-09-08 13:27:32 +0700

    add variable.js file

---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+// function foo() {
+//     for (var i = 0; i < 4; i++) {
+//         console.log("easy learning");
+//     }
+//     console.log(i);
+// }
+// foo();
+function hihi([a,b,...rest]){
+    console.log(a);
+    console.log(b);
+    console.log(rest);
+
+} 
+hihi([1,2,3,4,45,3]);
