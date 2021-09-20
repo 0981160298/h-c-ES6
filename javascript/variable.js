d75db2960a077fdc82dc139f794a00e98be9d7d9
0981160298 <62273292+0981160298@users.noreply.github.com>
2021-09-20 19:12:21 +0700

    update

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,15 +1,5 @@
-// function foo() {
-//     for (var i = 0; i < 4; i++) {
-//         console.log("easy learning");
-//     }
-//     console.log(i);
-// }
-// foo();
 
-
-
-
-// rest (trả về dạng mảng)
+//! rest (trả về dạng mảng)
 
 // function hihi([a,b,...rest]){
 //     console.log(a);
@@ -18,9 +8,30 @@
 // } 
 // hihi([1,2,3,4,45,3]);
 
+//! spread (spread giống ress nhưng nó bỏ đi dấu ngoặc trước array hoặc oject)
 
 
-// Tagged template literals
+// var array1 =['javascript','ruby','php'];
+// var array2 =['ReactJS','Dart'];
+// var array3 =[...array1,...array2]
+
+// console.log(array3);
+
+
+// var object1={
+//     name:'javascript'
+// };
+// var object2={
+//     price:'1000'
+// };
+// var object3={
+//     ...object1,...object2
+// };
+// console.log(object3);
+
+
+
+//! Tagged template literals
 
 // function hightlight([first,...strings],...valuses){
 //     return valuses.reduce(
@@ -38,7 +49,7 @@
 
 
 
-// dùng reduce tìm min max
+//! dùng reduce tìm min max
 
 // const arr =['1','2','5','6'];
 // const minList= arr.reduce((a,b) =>{
@@ -49,7 +60,7 @@
 // console.log(minList);
 
 
-//tìm số chắn lớn nhất
+//!tìm số chắn lớn nhất
 
 // const arr=['9','2','4','3'];
 // const sochanmax=arr.filter(a=>a%2==0).reduce((a,b)=>{
@@ -61,7 +72,7 @@
 
 
 
- //viết hàm chuyển đổi mảng thành object và ngược lại sử dụng reduce ()
+ //!viết hàm chuyển đổi mảng thành object và ngược lại sử dụng reduce ()
 
 //  const itemMap={
 //      key1: 'Superman',
@@ -74,7 +85,7 @@
 //      {id: 'key1', value: 'Spider man'}
 //  ]
 
-//giải chuyển đổi từ object sang mảng
+//!giải chuyển đổi từ object sang mảng
 
 // const itemMap={
 //     key1: 'Superman',
@@ -92,7 +103,7 @@
 // };
 // console.log(object(itemMap));
 
-// giải chuyển đổi từ mảng sang object
+//! giải chuyển đổi từ mảng sang object
 
 // const itemList=[
 //     {id: 'key1', value: 'Superman'},
@@ -109,7 +120,7 @@
 
 
 
-//destructuring
+//!destructuring
 
 //vd đơn giản
 // var array=['hihi','haha','huhu'];
@@ -118,7 +129,7 @@
 // console.log(rest);
 
 
-// // vd 2
+//! vd 2
 // var course={
 //     name : 'javascript',
 //     price: '1000',
@@ -126,18 +137,20 @@
 //     children:{
 //         name:'ReactJS'
 //     },
-//     // description:'description value'
+    // (description:'description value')
 
 // };
-//  // nếu trùng name thì phải đặt tên khác  
-//  // nếu description có thì in ra mà k có thì lấy 'hihi'
+//  ( nếu trùng name thì phải đặt tên khác)  
+//  ( nếu description có thì in ra mà k có thì lấy 'hihi')
 // var { name: parentName,description='hihi',price, children:{name:childrenName }}=course;
 // console.log(parentName,price);
 // console.log(description);
 // console.log(childrenName);
 
 
-// classes trong es6
+//! classes trong es6
+
+//vd1
 
 // class course{
 //     constructor (name,price){
@@ -153,6 +166,84 @@
 
 // console.log(phpCourse);
 // console.log(jsCourse);
+
+//vd2
+
+// class TraiCay{
+//     constructor(ten){
+//         this.ten=ten;
+//         this.getHihi=function(){
+//             return this.ten +" helo ae ";
+//         }
+//     }
+//     ngon(){
+//         console.log(this.ten + ' xoài ngon lắm nè ');
+//     }
+//     static hihi(){
+//         console.log("ok static");
+//     }
+    
+
+// }
+// let add = new TraiCay('ok');
+// add.ngon();
+// console.log(add.getHihi());
+
+
+
+//!default parameter values
+
+// function logger (log='giá trị mạc định'){
+//     console.log(log);
+// }
+// logger('hihi');
+
+
+
+//!Enhanced object literals
+
+// var name ='javascript';
+// var price='1000';
+
+// trước
+// var course={
+//     name:name,
+//     price:price,
+//     getName: function(){
+//     return name;
+// }
+// }
+
+//sau khi áp dụng
+// var course={
+//     name,
+//     price,
+//     getName(){
+//         return name;
+//     }
+// }
+// console.log(course.getName());
+
+
+// //! Modules: import/export (chưa xong)
+
+// import logger from './logger.js';
+
+// console.log(logger)
+
+
+//! this Keyword of javascript
+
+// const Mango={
+//     color:'Yellow',
+//     price: '100',
+//     weight: '130',
+//     about: function(){
+//         (//return 'hihi ' + this.price+ ' mãi yêu'; )
+//         return ` hihi ${this.color} mãi yêu`;
+//     }
+// }
+// console.log(Mango.about());
 
 
 //    node javascript/variable.js
